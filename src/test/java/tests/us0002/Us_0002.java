d01f21242e0ad8bd9d43b1d8e177e4d68fba0b1d
dryigit <fatihyigit6@gmail.com>
2022-02-21 14:55:41 +0300

    us0002-myigit raporlama

--- src/test/java/tests/us0002/Us_0002.java
+++ src/test/java/tests/us0002/Us_0002.java
@@ -6,15 +6,23 @@ import pages.LoginPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
+import utilities.TestBaseRapor;
 
-public class US0002 {
+import java.io.IOException;
+
+
+public class Us_0002 extends TestBaseRapor {
 HotelManagerPage hotelManagerPage=new HotelManagerPage();
 LoginPage loginPage=new LoginPage();
 
 @Test
-public void testCase01 (){
+public void testCase01 () throws IOException {
 	//Kullanıcı https://www.hotelmycamp.com adresine erişebilmeli
-Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
+	
+	extentTest=extentReports.createTest("US0002 Rapor","HotelMyCamp websayfası'na gidildiği test edildi");
+	Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
+	extentTest.pass("HMC sayfasına gidildi");
+	
 }
 
 @Test
@@ -22,33 +30,40 @@ public void testCase02 (){
 	//Kullanıcı menüde bulunan Log in butonuna tıklayabilmeli
 	//Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
 	loginPage.ilkSayfaLoginButtonWebElement.click();
-
+	extentTest.pass("Menüdeki Log in butonuna tıklandı");
+	
 }
 @Test
 public void testCase03 (){
 	//Kullanıcı log in sayfasında doğru username girebilmeli
 	loginPage.ilkSayfaLoginButtonWebElement.click();
 	loginPage.usernameWebElement.sendKeys(ConfigReader.getProperty("HMCValidUsername"));
-
+	extentTest.pass("Doğru username girildi");
+	
+	
 }
 @Test
 public void testCase04 (){
 	//Kullanıcı log in sayfasında doğru password girebilmeli
 	loginPage.passwordWebElement.sendKeys(ConfigReader.getProperty("HMCValidPassword"));
+	extentTest.pass("Doğru password girildi");
+	
 	
 }
 @Test
 public void testCase05 (){
 	//Kullanıcı login butonuna tıklayabilmeli
 	loginPage.accountLoginWebElement.click();
+	extentTest.pass("Log in butonuna tıklandı");
+	
 	
 }
 @Test
 public void testCase06 (){
 	//Kullanıcı başarılı bir şekilde giriş yapabilmeli
-	ReusableMethods.waitFor(3);
+	ReusableMethods.waitFor(4);
 	Assert.assertTrue(hotelManagerPage.hotelManagerButonuWebElement.isDisplayed());
+	extentTest.pass("Kullanıcı başarılı bir şekilde giriş yaptı");
 	
 }
-
 }
